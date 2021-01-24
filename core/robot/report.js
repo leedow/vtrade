ad249e4891615eb834bc2e9459e9dd5ef8ca4cf1
leedow <644743991@qq.com>
2021-01-24 11:03:37 +0800

    report强化

--- core/robot/report.js
+++ core/robot/report.js
@@ -8,7 +8,46 @@ module.exports = class Report extends Core{
     super()
     this.copyOptions(options)
     // this.x = [] // 记录时间轴
-    this.content = {x: []} // 报告数据 {a: [], b: []}
+    this._content = []
+    //this._content = {x: []} // 报告数据 {a: [], b: []}
+  }
+
+
+  /*
+   * @return {x: [], a: [], b: []}
+   */
+  get content() {
+    return this._format(this._content)
+  }
+
+  /*
+   * 将[{x,a,b}...]转化为{x:[],a:[]...}
+   */
+  _format(datas) {
+    if(datas.length == 0) return {}
+
+    let res = {
+      x: []
+    }
+
+    Object.keys(datas[0]['d']).forEach(key => {
+      if(!res[key]) {
+        res[key] = []
+      }
+    })    
+
+    for (var i = 0; i < datas.length; i++) {
+      let item = datas[i]
+      res.x.push(item.x)
+
+      Object.keys(item.d).forEach(key => {
+        if(res[key]) {
+          res[key].push(item['d'][key])
+        }
+      })    
+    }
+
+    return res
   }
 
   /**
@@ -17,16 +56,66 @@ module.exports = class Report extends Core{
    * @param data {a:value, b:value}
    */
   record(x, data) {
-    this.content.x.push(x)
-    //console.log(data)
-    Object.keys(data).forEach(key => {
-      if(this.content[key]) {
-        this.content[key].push(data[key])
-      } else {
-        this.content[key] = [data[key]]
-      }
-
+    this._content.push({
+      x,
+      d: data
     })
+    // this.content.x.push(x)
+    // //console.log(data)
+    // Object.keys(data).forEach(key => {
+    //   if(this.content[key]) {
+    //     this.content[key].push(data[key])
+    //   } else {
+    //     this.content[key] = [data[key]]
+    //   }
+    // })
+  }
+
+  /*
+   * 初始化历史数据
+   * @data {x: [], a: [], b: []}
+   */
+  restore(data) {
+    let keys = Object.keys(data)
+    this._content = []
+
+    data.x.forEach((x, index) => {
+      let d = {}
+
+      keys.forEach(key => {
+        if(key != 'x') d[key] = data[key][index]
+      })
+
+      this.record(x, d)
+    })
+  }
+
+  /*
+   * 压缩数据，限定数据长度总量，等比距离取样
+   * 返回新数据，不改变旧数据
+   * @limit 限定的总记录数
+   */ 
+  compress() {
+
+  }
+
+  /*
+   * 按条件压缩数据长度
+   * 返回新数据，不改变旧数据
+   * @compare (current, pre) 对比回调函数,返回true保留当前current数据，返回false丢弃当前current数据
+   */
+  compressBy(compare) {
+    let result = []
+    this._content.forEach( (item, index) => {
+      if(index > 0) {
+        if( compare(item, result[result.length-1]) ) {
+          result.push(item)
+        }
+      } else {
+        result.push(item)
+      }
+    })
+    return this._format(result)
   }
 
   result() {
--- test/robot/report.js
+++ test/robot/report.js
@@ -16,6 +16,51 @@ describe('测试report模块',function(){
       b: [2],
       c: [3]
     })
+  })
+
+  it('restore',function(){
+    const data = {
+      x: [1,2],
+      a: [1,3],
+      b: [2,4],
+      c: [3,6]
+    }
+
+    report.restore(data)
+    assert.deepEqual( report.result(), data)
+  })
+
+  it('compressBy',function(){
+    const data = {
+      x: [1,2,6,10],
+      a: [1,3,1,1],
+      b: [2,4,2,2],
+      c: [3,6,3,3]
+    }
+
+    report.restore(data)
+    assert.deepEqual( report.compressBy((current, pre) => {
+      return current.x - pre.x >= 1
+    }), data)
+
+    assert.deepEqual( report.compressBy((current, pre) => {
+      return current.x - pre.x > 1
+    }), {
+      x: [1,6,10],
+      a: [1,1,1],
+      b: [2,2,2],
+      c: [3,3,3]
+    })
+
+    assert.deepEqual( report.compressBy((current, pre) => {
+      return current.x - pre.x > 5
+    }), {
+      x: [1,10],
+      a: [1,1],
+      b: [2,2],
+      c: [3,3]
+    })
+
 
   })
 })

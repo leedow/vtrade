7b9e66b0becf8ec27d07c764c855c0a9c11f9a7f
644743991@qq.com <644743991@qq.com>
2022-07-21 12:03:50 +0800

    移除tablib
    移除tablib

--- test/feed/kline/talib.js
+++ test/feed/kline/talib.js
@@ -3,86 +3,86 @@ var Kline = require('../../../core/feed/kline')
 var Tickers = require('../../../core/feed/tickers')
 var helper = require('../../../core/tools/helper')
 
-describe('测试feed/kline模块指标计算',function(){
-  let kline = new Kline()
-  let tickers = new Tickers()
+// describe('测试feed/kline模块指标计算',function(){
+//   let kline = new Kline()
+//   let tickers = new Tickers()
 
-  kline.filterSame = false
-  kline.memoryTimeLimit = 99999999999999999
-  kline.ktype = 60
-  tickers.filterSame = false
-  tickers.memoryTimeLimit = 99999999999999999
+//   kline.filterSame = false
+//   kline.memoryTimeLimit = 99999999999999999
+//   kline.ktype = 60
+//   tickers.filterSame = false
+//   tickers.memoryTimeLimit = 99999999999999999
 
-  let now = Date.now()
-  let n = 0
-  const nextTime = () => {
-    now =  now + parseInt(10000*Math.random())
-    return now
-  }
+//   let now = Date.now()
+//   let n = 0
+//   const nextTime = () => {
+//     now =  now + parseInt(10000*Math.random())
+//     return now
+//   }
 
-  const randomPrice = () => {
-    return Number ( (1000*Math.random()).toFixed(2)  )
-  }
+//   const randomPrice = () => {
+//     return Number ( (1000*Math.random()).toFixed(2)  )
+//   }
 
-  const getK = (size=10000) => {
-    let res = []
-    while(size--) {
-      res.push([randomPrice(), 0,0,0,0,0,0,0,0,0,0, nextTime()])
-      n++
-    }
-    return res
-  }
+//   const getK = (size=10000) => {
+//     let res = []
+//     while(size--) {
+//       res.push([randomPrice(), 0,0,0,0,0,0,0,0,0,0, nextTime()])
+//       n++
+//     }
+//     return res
+//   }
 
-  let k = getK()
+//   let k = getK()
 
-  k.forEach(item => {
-    tickers.remember(item, item[11])
-  })
+//   k.forEach(item => {
+//     tickers.remember(item, item[11])
+//   })
 
-  kline.transTickers(tickers)
+//   kline.transTickers(tickers)
 
-  it('RSI',function(){
-    assert.equal( typeof kline.RSI(14),  'number' )
-  })
+//   it('RSI',function(){
+//     assert.equal( typeof kline.RSI(14),  'number' )
+//   })
 
-  it('RSI size 2',function(){
-    assert.equal( kline.RSI(14, 2).length,  2 )
-  })
+//   it('RSI size 2',function(){
+//     assert.equal( kline.RSI(14, 2).length,  2 )
+//   })
 
-  it('MA',function(){
-    assert.equal( typeof kline.MA(14),  'number' )
-  })
+//   it('MA',function(){
+//     assert.equal( typeof kline.MA(14),  'number' )
+//   })
 
-  it('ma',function(){
-    assert.equal( kline.MA(14),  kline.ma(14) )
-  })
+//   it('ma',function(){
+//     assert.equal( kline.MA(14),  kline.ma(14) )
+//   })
 
-  it('EMA',function(){
-    assert.equal( typeof kline.EMA(14),  'number' )
-  })
+//   it('EMA',function(){
+//     assert.equal( typeof kline.EMA(14),  'number' )
+//   })
 
-  it('ema',function(){
-    console.log(1111, kline.data.length)
-    let st = Date.now()
-    assert.equal(  kline.ema(200),  kline.ema(200) )
-    console.log(2222, Date.now()-st)
-  })
+//   it('ema',function(){
+//     console.log(1111, kline.data.length)
+//     let st = Date.now()
+//     assert.equal(  kline.ema(200),  kline.ema(200) )
+//     console.log(2222, Date.now()-st)
+//   })
 
-  it('SMA',function(){
-    assert.equal( typeof kline.SMA(14),  'number' )
-  })
+//   it('SMA',function(){
+//     assert.equal( typeof kline.SMA(14),  'number' )
+//   })
 
-  it('SMA size 2',function(){
-    assert.equal(  kline.SMA(14,2).length,  2 )
-  })
+//   it('SMA size 2',function(){
+//     assert.equal(  kline.SMA(14,2).length,  2 )
+//   })
 
-  it('ATR',function(){
-    assert.equal( typeof kline.ATR(14),  'number' )
-  })
+//   it('ATR',function(){
+//     assert.equal( typeof kline.ATR(14),  'number' )
+//   })
 
-  it('ATR size 2',function(){
-    assert.equal(  kline.ATR(14,2).length,  2 )
-  })
+//   it('ATR size 2',function(){
+//     assert.equal(  kline.ATR(14,2).length,  2 )
+//   })
 
 
-})
+// })
--- test/feed/kline/talibReal.js
+++ test/feed/kline/talibReal.js
@@ -4,221 +4,221 @@ var Tickers = require('../../../core/feed/tickers')
 var helper = require('../../../core/tools/helper')
 let talib = require('talib')
 
-describe('测试feed/kline模块指标计算，核对和talib直接计算结果',function(){
-  let kline = new Kline()
+// describe('测试feed/kline模块指标计算，核对和talib直接计算结果',function(){
+//   let kline = new Kline()
   
-  kline.filterSame = false
-  kline.memoryTimeLimit = 99999999999999999
-  kline.ktype = 60
+//   kline.filterSame = false
+//   kline.memoryTimeLimit = 99999999999999999
+//   kline.ktype = 60
 
-  let st = Date.now()
+//   let st = Date.now()
 
 
-  const ks = [{
-    id: st,
-    high: 100,
-    low: 1,
-    open: 50,
-    close: 40,
-    stime: st,
-    etime: st + 59999
-  },{
-    id: st + 60000,
-    high: 200,
-    low: 15,
-    open: 40,
-    close: 70,
-    stime: st + 60000,
-    etime: st + 60000 + 59999
-  },{
-    id: st + 2*60000,
-    high: 300,
-    low: 15,
-    open: 70,
-    close: 70,
-    stime: st + 2*60000,
-    etime: st + 2*60000 + 59999
-  },{
-    id: st + 3*60000,
-    high: 320,
-    low: 11,
-    open: 72,
-    close: 77,
-    stime: st + 3*60000,
-    etime: st + 3*60000 + 59994
-  }]
+//   const ks = [{
+//     id: st,
+//     high: 100,
+//     low: 1,
+//     open: 50,
+//     close: 40,
+//     stime: st,
+//     etime: st + 59999
+//   },{
+//     id: st + 60000,
+//     high: 200,
+//     low: 15,
+//     open: 40,
+//     close: 70,
+//     stime: st + 60000,
+//     etime: st + 60000 + 59999
+//   },{
+//     id: st + 2*60000,
+//     high: 300,
+//     low: 15,
+//     open: 70,
+//     close: 70,
+//     stime: st + 2*60000,
+//     etime: st + 2*60000 + 59999
+//   },{
+//     id: st + 3*60000,
+//     high: 320,
+//     low: 11,
+//     open: 72,
+//     close: 77,
+//     stime: st + 3*60000,
+//     etime: st + 3*60000 + 59994
+//   }]
 
-  const ks2 = [ks[0], ks[1], ks[2]]
+//   const ks2 = [ks[0], ks[1], ks[2]]
 
-  ks.forEach(k => {
-    kline.remember(k)
-  })
+//   ks.forEach(k => {
+//     kline.remember(k)
+//   })
 
-  it('MA',function(){
-    let real1 = talib.execute({
-        name: 'MA',
-        startIdx: ks.length-1,
-        endIdx: ks.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks.map(item => item.close)
-    })
+//   it('MA',function(){
+//     let real1 = talib.execute({
+//         name: 'MA',
+//         startIdx: ks.length-1,
+//         endIdx: ks.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks.map(item => item.close)
+//     })
 
-    let real2 = kline.MA(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//     let real2 = kline.MA(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-  it('ma',function(){
-    let real1 = talib.execute({
-        name: 'MA',
-        startIdx: ks.length-1,
-        endIdx: ks.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks.map(item => item.close)
-    })
+//   it('ma',function(){
+//     let real1 = talib.execute({
+//         name: 'MA',
+//         startIdx: ks.length-1,
+//         endIdx: ks.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks.map(item => item.close)
+//     })
 
-    let real2 = kline.ma(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//     let real2 = kline.ma(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
  
-  it('EMA',function(){
-    let real1 = talib.execute({
-        name: 'EMA',
-        startIdx: ks.length-1,
-        endIdx: ks.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks.map(item => item.close)
-    })
+//   it('EMA',function(){
+//     let real1 = talib.execute({
+//         name: 'EMA',
+//         startIdx: ks.length-1,
+//         endIdx: ks.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks.map(item => item.close)
+//     })
 
-    let real2 = kline.EMA(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//     let real2 = kline.EMA(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-  // 40 70 70 77
-  // it('ema',function(){
-  //   let real1 = talib.execute({
-  //       name: 'EMA',
-  //       startIdx: ks.length-1,
-  //       endIdx: ks.length-1,
-  //       optInTimePeriod: 2,
-  //       optInMAType: 1,
-  //       inReal: ks.map(item => item.close)
-  //   })
+//   // 40 70 70 77
+//   // it('ema',function(){
+//   //   let real1 = talib.execute({
+//   //       name: 'EMA',
+//   //       startIdx: ks.length-1,
+//   //       endIdx: ks.length-1,
+//   //       optInTimePeriod: 2,
+//   //       optInMAType: 1,
+//   //       inReal: ks.map(item => item.close)
+//   //   })
 
-  //   let real2 = kline.ema(2, 1)
-  //   assert.equal( real1.result.outReal[0], real2 )
-  // })
+//   //   let real2 = kline.ema(2, 1)
+//   //   assert.equal( real1.result.outReal[0], real2 )
+//   // })
 
-  it('RSI',function(){
-    let real1 = talib.execute({
-        name: 'RSI',
-        startIdx: ks.length-1,
-        endIdx: ks.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks.map(item => item.close)
-    })
+//   it('RSI',function(){
+//     let real1 = talib.execute({
+//         name: 'RSI',
+//         startIdx: ks.length-1,
+//         endIdx: ks.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks.map(item => item.close)
+//     })
 
-    let real2 = kline.RSI(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//     let real2 = kline.RSI(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-  it('ATR',function(){
-    let real1 = talib.execute({
-        name: 'ATR',
-        startIdx: ks.length-1,
-        endIdx: ks.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks.map(item => item.close),
-        high: ks.map(item => item.high),
-        low: ks.map(item => item.low),
-        open: ks.map(item => item.open),
-        close: ks.map(item => item.close)
-    })
+//   it('ATR',function(){
+//     let real1 = talib.execute({
+//         name: 'ATR',
+//         startIdx: ks.length-1,
+//         endIdx: ks.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks.map(item => item.close),
+//         high: ks.map(item => item.high),
+//         low: ks.map(item => item.low),
+//         open: ks.map(item => item.open),
+//         close: ks.map(item => item.close)
+//     })
 
-    let real2 = kline.ATR(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//     let real2 = kline.ATR(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-  it('MA with ignoreIncomplete',function(){
-    let real1 = talib.execute({
-        name: 'MA',
-        startIdx: ks2.length-1,
-        endIdx: ks2.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks2.map(item => item.close)
-    })
-    kline.ignoreIncomplete = true
-    let real2 = kline.MA(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//   it('MA with ignoreIncomplete',function(){
+//     let real1 = talib.execute({
+//         name: 'MA',
+//         startIdx: ks2.length-1,
+//         endIdx: ks2.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks2.map(item => item.close)
+//     })
+//     kline.ignoreIncomplete = true
+//     let real2 = kline.MA(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-  it('ma with ignoreIncomplete',function(){
-    let real1 = talib.execute({
-        name: 'MA',
-        startIdx: ks2.length-1,
-        endIdx: ks2.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks2.map(item => item.close)
-    })
-    kline.ignoreIncomplete = true
-    let real2 = kline.ma(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
-
-
-  it('EMA with ignoreIncomplete',function(){
-    let real1 = talib.execute({
-        name: 'EMA',
-        startIdx: ks2.length-1,
-        endIdx: ks2.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks2.map(item => item.close)
-    })
-    kline.ignoreIncomplete = true
-    let real2 = kline.EMA(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
-
-  it('RSI with ignoreIncomplete',function(){
-    let real1 = talib.execute({
-        name: 'RSI',
-        startIdx: ks2.length-1,
-        endIdx: ks2.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks2.map(item => item.close)
-    })
-    kline.ignoreIncomplete = true
-    let real2 = kline.RSI(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
-
-  it('ATR with ignoreIncomplete',function(){
-    let real1 = talib.execute({
-        name: 'RSI',
-        startIdx: ks2.length-1,
-        endIdx: ks2.length-1,
-        optInTimePeriod: 2,
-        optInMAType: 1,
-        inReal: ks2.map(item => item.close),
-        high: ks2.map(item => item.high),
-        low: ks2.map(item => item.low),
-        open: ks2.map(item => item.open),
-        close: ks2.map(item => item.close)
-    })
-    kline.ignoreIncomplete = true
-    let real2 = kline.RSI(2, 1)
-    assert.equal( real1.result.outReal[0], real2 )
-  })
+//   it('ma with ignoreIncomplete',function(){
+//     let real1 = talib.execute({
+//         name: 'MA',
+//         startIdx: ks2.length-1,
+//         endIdx: ks2.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks2.map(item => item.close)
+//     })
+//     kline.ignoreIncomplete = true
+//     let real2 = kline.ma(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
 
+//   it('EMA with ignoreIncomplete',function(){
+//     let real1 = talib.execute({
+//         name: 'EMA',
+//         startIdx: ks2.length-1,
+//         endIdx: ks2.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks2.map(item => item.close)
+//     })
+//     kline.ignoreIncomplete = true
+//     let real2 = kline.EMA(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
 
-})
+//   it('RSI with ignoreIncomplete',function(){
+//     let real1 = talib.execute({
+//         name: 'RSI',
+//         startIdx: ks2.length-1,
+//         endIdx: ks2.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks2.map(item => item.close)
+//     })
+//     kline.ignoreIncomplete = true
+//     let real2 = kline.RSI(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
+
+//   it('ATR with ignoreIncomplete',function(){
+//     let real1 = talib.execute({
+//         name: 'RSI',
+//         startIdx: ks2.length-1,
+//         endIdx: ks2.length-1,
+//         optInTimePeriod: 2,
+//         optInMAType: 1,
+//         inReal: ks2.map(item => item.close),
+//         high: ks2.map(item => item.high),
+//         low: ks2.map(item => item.low),
+//         open: ks2.map(item => item.open),
+//         close: ks2.map(item => item.close)
+//     })
+//     kline.ignoreIncomplete = true
+//     let real2 = kline.RSI(2, 1)
+//     assert.equal( real1.result.outReal[0], real2 )
+//   })
+
+
+
+// })
